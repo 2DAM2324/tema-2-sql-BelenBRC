c80841806c2139ee88d124feafeb1ddc4a49dbcb
BelenBRC <belenrob.alumn@cescristorey.com>
2023-12-03 21:21:00 +0100

    Tests de la clase valoración

--- rutas/src/test/java/Modelo/ValoracionTest.java
+++ rutas/src/test/java/Modelo/ValoracionTest.java
@@ -12,6 +12,10 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author belen
  */
 public class ValoracionTest {
+
+    Valoracion instance;
+    Usuario usuario;
+    Ruta ruta;
     
     public ValoracionTest() {
     }
@@ -32,20 +36,36 @@ public class ValoracionTest {
     public void tearDown() {
     }
 
-    //TODO implementar tests que sean necesarios
-
     /**
      * Test of toString method, of class Valoracion.
      */
     @Test
     public void testToString() {
         System.out.println("toString");
-        Valoracion instance = null;
-        String expResult = "";
+        usuario = new Usuario("UsuarioPrueba", "Prueba", "Prueba", "tu@tu.es", "1234", "12345678Z");
+        ruta = new Ruta("Ruta de prueba", "Descripción de prueba", 1, "ALTA", 1, usuario);
+        instance = new Valoracion(ruta, usuario, 5, "Comentario de prueba");
+
+        String expResult = "Puntuación: 5\nComentario: Comentario de prueba";
         String result = instance.toString();
+
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
+
+    /**
+     * Test of toString method, of class Valoracion.
+     * Comentario vacío
+     */
+    @Test
+    public void testToString2() {
+        System.out.println("toString");
+        usuario = new Usuario("UsuarioPrueba", "Prueba", "Prueba", "tu@tu.es", "1234", "12345678Z");
+        ruta = new Ruta("Ruta de prueba", "Descripción de prueba", 1, "ALTA", 1, usuario);
+        instance = new Valoracion(ruta, usuario, 5, "");
+
+        String expResult = "Puntuación: 5\nComentario: ";
+        String result = instance.toString();
+
+        assertEquals(expResult, result);
+    }
 }
